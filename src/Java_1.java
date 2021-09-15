5891706a0974adaac07a52f01d5da4e0d8029bb4
Masheba <nikolaiageev1991@gmail.com>
2021-09-15 11:22:47 +0300

    test report

--- src/Java_1.java
+++ src/Java_1.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         int account_balance = 2_000_000_000 ;
         int transfer_amount = 500_000_000;
-        int total = account_balance - transfer_amount;
+        int total = account_balance + transfer_amount;
         System.out.println(total);
     }
 }
